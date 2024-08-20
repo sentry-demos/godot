a25eb2e1aa5ee6ecd220ce4eecfe47a42864fb50
bitsandfoxes <reg@bitfox.at>
2024-08-20 11:04:53 +0200

    bump

--- src/game.gd
+++ src/game.gd
@@ -3,7 +3,7 @@ extends Node
 @onready var player = $Player
 
 func _init():
-	var sentry = SentryExample.new()
+	SentrySdk.captureMessasge()
 
 func _process(_delta: float) -> void:	
 	if Input.is_action_just_pressed("ui_fullscreen"):
