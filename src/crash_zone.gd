568a5631f9ccf6d53a6845bb34e48f859a4a22bc
Serhii Snitsaruk <limbonaut@pm.me>
2025-05-13 12:22:18 +0200

    Update and fix demo stuff

--- src/crash_zone.gd
+++ src/crash_zone.gd
@@ -2,5 +2,5 @@ extends Area2D
 
 
 func _on_area_entered(_area: Area2D) -> void:
-	# Calling into the native code that crashes application.
+	print("Calling into native code that crashes the game.")
 	SentrySDK._demo_helper_crash_app()
--- src/goal.gd
+++ src/goal.gd
@@ -1,11 +1,11 @@
 extends Area2D
 
-@export var victory_animation: Node
+@export var animation_player: AnimationPlayer
 
 
 func _play_victory_animation():
-	# Results in error as victory_animation is not initialized or assigned.
-	victory_animation.play()
+	# Results in error as "victory" animation doesn't exist.
+	animation_player.play("victory")
 
 
 func _on_body_entered(_player: CharacterBody2D):
